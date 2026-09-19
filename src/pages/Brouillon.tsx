fbc32db3912dda5056b813f0b9f2d2bd35f0cbed
Fred <fredichampix@gmail.com>
2026-09-19 14:57:45 +0200

    Memory Card finit.

--- src/pages/Brouillon.tsx
+++ src/pages/Brouillon.tsx
@@ -1,4 +1,16 @@
+import { NavLink } from "react-router";
 
 export default function Brouillon() {
-  return <div>Brouillon</div>
+  return <Exercice/>;
+}
+
+function Exercice() {
+  return (
+    <div className="pt-16">
+      <NavLink end className={ ({isActive}) => isActive ? "border p-2 text-blue-600" : "border p-2"} to='/mon-compte'>1</NavLink>
+      <NavLink end className={ ({isActive}) => isActive ? "border p-2 text-blue-600" : "border p-2"} to='/mon-compte/commande'>2</NavLink>
+      <NavLink className={ ({isActive}) => isActive ? "border p-2 text-blue-600" : "border p-2"} to='/mon-compte/commande/:id'>3</NavLink>
+      <NavLink className={ ({isActive}) => isActive ? "border p-2 text-blue-600" : "border p-2"} to='/mon-compte/ordonnance'>4</NavLink>
+    </div>
+  )
 }
